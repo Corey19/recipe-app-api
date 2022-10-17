4f479b583fa3a4dfff0db118d79acf6c49e6bc6a
corey19 <coreyziglar@gmail.com>
2022-10-17 12:15:47 -0400

    postgres dependencies added

---
+++ app/app/test.py
@@ -0,0 +1,14 @@
+"""
+sample tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class calcTest(SimpleTestCase):
+    def test_add_numbers(self):
+        
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
